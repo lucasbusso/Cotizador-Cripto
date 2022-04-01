e0240fba05c929b190f3a431fbfeebd6b2c6b6cf
Lucas Busso <lucasxbusso@gmail.com>
2022-03-31 23:14:45 -0300

    Componente Spinner de tobias ahlin spinkit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import Formulario from './components/Formulario'
 import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
 import ImagenCripto from './img/imagen-criptos.png'
 
 const Contenedor = styled.div `
@@ -46,10 +47,14 @@ function App() {
  
   const [ monedas, setMonedas ] = useState({})
   const [ resultado, setResultado ] = useState({})
+  const [ cargando, setCargando ] = useState(false)
 
   useEffect( () => {
     if(Object.keys(monedas).length > 0){
+
       const cotizarCripto = async () => {
+        setCargando(true)
+        setResultado({})
         const { moneda, criptomoneda } = monedas 
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
       
@@ -57,6 +62,8 @@ function App() {
         const resultado = await respuesta.json()
 
         setResultado(resultado.DISPLAY[criptomoneda][moneda])
+
+        setCargando(false)
       }
 
       cotizarCripto()
@@ -76,6 +83,7 @@ function App() {
           setMonedas={setMonedas}
         />
 
+        { cargando && <Spinner/>}
         { resultado.PRICE && <Resultado resultado={resultado} />}
       </div>
 
